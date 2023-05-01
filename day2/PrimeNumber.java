c905734a2eea81782fb5b3e560e5e713c81f5ff6
Derick Cruz <67696451+dcrcruz@users.noreply.github.com>
2023-05-01 20:32:32 +0800

    Final update of PrimeNumber.java

--- day2/PrimeNumber.java
+++ day2/PrimeNumber.java
@@ -1,9 +1,30 @@
 import java.util.Scanner;
-import java.util.Random;
 
-class PrimeNumber {
-
+class Prime {
     public static void main(String[] args) {
-      
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to check if it is a prime number: ");
+        int number = sc.nextInt();
+        if(isPrime(number)) {
+            System.out.println(number + " is a prime number");
+        }
+        else{
+            System.out.println(number + " is not a prime number");
+        }
     }
+    static boolean isPrime(int num)
+    {
+        if(num<=1)
+        {
+            return false;
+        }
+       for(int i=2;i<=num/2;i++)
+       {
+           if((num%i)==0)
+               return  false;
+       }
+       return true;
+    }
+
+
 }
