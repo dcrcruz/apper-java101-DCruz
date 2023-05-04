4e5a806fd6bfa3a2004fedcee7ae9fc3d6629c33
Derick Cruz <67696451+dcrcruz@users.noreply.github.com>
2023-05-04 18:40:39 +0800

    Create HW3.java

---
+++ day3/HW3.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class OddEven {
+ public static void main(String[] args)
+ {
+    int[] nums = {1, 234, 2, 4, 56, 17, 50, 1235, 6, 1, 23, 48, 47, 34};
+	int ctr_even = 0, ctr_odd = 0;
+	System.out.println("Original Array: "+Arrays.toString(nums)); 
+	    
+    for(int i = 0; i < nums.length; i++) {
+        if(nums[i] % 2 == 0)
+		{         
+          ctr_even++;
+		}
+		else
+		   ctr_odd++;	
+    }                 
+    System.out.printf("\nOdd: %d",ctr_odd);
+	System.out.printf("\nEven: %d",ctr_even);
+	System.out.printf("\n");	
+  }
+}
